1f4c0c02c3c41213ce2bff49c9064ef831629f94
vertixlaw <vertixlaw@gmail.com>
2025-10-10 14:52:24 -0400

    Delete src directory

--- src/components/Clients.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Logos3 } from './ui/logos3'
-
-const Clients = () => {
-  return <Logos3 />
-}
-
-export default Clients
--- src/components/Plans.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import PricingSection4 from './ui/pricing-section-4'
-
-const Plans = () => {
-  return <PricingSection4 />
-}
-
-export default Plans
